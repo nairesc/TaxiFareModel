4d8f372b2a69ad808e6494588774fec6439d64e7
Nuria Aires <nuria.airesc@gmail.com>
2021-11-17 11:23:15 +0000

    something done

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,3 +1,4 @@
+from mlflow.entities import experiment
 from TaxiFareModel.data import get_data, clean_data
 from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
@@ -7,9 +8,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = "[UK] [London] [nairesc] TaxiFareModelv1"
 
 class Trainer():
+
     def __init__(self, X, y):
         """
             X: pandas DataFrame
@@ -18,6 +25,11 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
+
+    def set_experiment_name(self, experiment_name):
+        '''defines the experiment name for MLFlow'''
+        self.experiment_name = experiment_name
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -42,16 +54,42 @@ class Trainer():
     def run(self):
         """set and train the pipeline"""
         self.set_pipeline()
+        self.mlflow_log_param("model", 'linear_regression')
         self.pipeline.fit(self.X, self.y)
-        pass
+
+        experiment_id = trainer.mlflow_experiment_id
+        print(
+            f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}"
+        )
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
-        print(rmse)
-        return rmse
+        self.mlflow_log_metric("rmse", rmse)
+        return round(rmse, 2)
 
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
